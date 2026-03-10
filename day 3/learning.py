d6911922c7c5cc980881e67577de246974abf6eb
Naseem Bajwa <naseembajwa@Naseems-MacBook-Pro.local>
2026-03-10 15:31:57 +0530

    naseem

---
+++ day 3/learning.py
@@ -0,0 +1,35 @@
+# # # # def square(number):
+# # # #     return number * number 
+# # # # result = (square (10))
+# # # # print (result)
+
+# # # # users = [ "admin", "guest", "guest", "user"] 
+
+# # # # for u in users:
+# # # #     if u == "guest":
+# # # #         users.remove(u)
+
+# # # # print (users)
+
+# # # print(7 % 2)
+ 
+# # # for i in range(1 ,11):
+# # #     print (i
+
+# # numbers = [5, 10, 15]
+# # numbers.insert(3, 20)
+# # print(numbers)
+
+
+# number = int(input("enter number here > "))
+
+# if number % 2 == 0 :
+#     print("even")
+# else:
+#     print ("odd")
+
+
+counter = 1
+while counter <= 5:
+    print (counter)
+    counter = counter + 1  
